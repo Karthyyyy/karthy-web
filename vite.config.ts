2e3a0f4d14402f77ab443832497526415322921b
Karthyyyy <d.mccarthyallen@gmail.com>
2022-07-22 01:07:32 +0100

    Getting server ready

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import VueTypeImports from 'vite-plugin-vue-type-imports'
 
 export default defineConfig({
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false
+        }
+      }
+    },
     server: {
       host: '0.0.0.0'
     },
@@ -12,4 +19,7 @@ export default defineConfig({
       vue(),
       VueTypeImports(),
     ],
+    build: {
+      minify: false
+    }
   })
